2e069df75c5940a2b94ce67329f666ae690e68c5
EricBermudezHernandez <91663286+EricBermudezHernandez@users.noreply.github.com>
2023-03-27 17:46:19 +0100

    Update sequence.hpp

--- sequence.hpp
+++ sequence.hpp
@@ -6,9 +6,9 @@
 template<class Key>
 class Sequence {
  public:  
-  bool Search(const Key& k) const = 0;
-  bool Insert(const Key& k) = 0;
-  bool IsFull() const = 0;
+  virtual bool Search(const Key& k) const = 0;
+  virtual bool Insert(const Key& k) = 0;
+  virtual bool IsFull() const = 0;
 };
 
 #endif
